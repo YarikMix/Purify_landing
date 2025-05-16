d8053924a1ee8ec852d5fd335be20517b2b3b3a9
YarikMix <43493788+YarikMix@users.noreply.github.com>
2025-05-17 01:11:52 +0300

    fix: Поправил моки

--- src/mocks.ts
+++ src/mocks.ts
@@ -3,17 +3,17 @@ import {siteItem} from "./types.ts";
 
 export const siteRatingMocks:siteItem[] = [
 	{
-		key: '1',
+		place: '1',
 		site: 'github.com',
 		aggressive_words: 32,
 	},
 	{
-		key: '2',
+		place: '2',
 		site: 'www.woman.ru',
 		aggressive_words: 42,
 	},
 	{
-		key: '3',
+		place: '3',
 		site: '2ch.hk',
 		aggressive_words: 4232,
 	},
